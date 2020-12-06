dca311e2026c8f195aadb73a5dc8046740f891f9
yarastqt <yarastqt@gmail.com>
2020-12-06 18:50:46 +0300

    feat: impl usePress hook

--- src/usePress/index.ts
+++ src/usePress/index.ts
@@ -1,1 +1,92 @@
-export function usePress() {}
+import React, { KeyboardEventHandler, MouseEvent, MouseEventHandler, useState } from 'react'
+
+type PressHookProps = {
+  disabled?: boolean
+}
+
+type PressHookResult = {
+  pressed: boolean
+  pressProps: {
+    onKeyDown: KeyboardEventHandler<HTMLElement>
+    onKeyUp: KeyboardEventHandler<HTMLElement>
+    onClick: MouseEventHandler<HTMLElement>
+    onMouseDown: MouseEventHandler<HTMLElement>
+    onMouseUp: MouseEventHandler<HTMLElement>
+    onMouseLeave: MouseEventHandler<HTMLElement>
+  }
+}
+
+// TODO: Состояние pressed может поступить снаружи.
+export function usePress({ disabled }: PressHookProps): PressHookResult {
+  const [pressed, setPressed] = useState(false)
+
+  // TODO: Все создаваемые функции должны быть обернуты в useCallback.
+  const onMouseDown = (event: MouseEvent<HTMLElement>) => {
+    // Предотвращаем потерю фокуса с элемента.
+    if (event.button === 0) {
+      event.preventDefault()
+    }
+
+    setPressed(true)
+  }
+
+  const onMouseUp = () => {
+    setPressed(false)
+  }
+
+  const onMouseLeave = () => {
+    setPressed(false)
+  }
+
+  const onClick = (event: MouseEvent<HTMLElement>) => {
+    if (event.button === 0) {
+      event.stopPropagation()
+      if (disabled) {
+        event.preventDefault()
+      } else {
+        event.currentTarget.focus({ preventScroll: true })
+      }
+    }
+  }
+
+  const onKeyDown = (event: React.KeyboardEvent<HTMLElement>) => {
+    if (isValidKeyboardEvent(event.nativeEvent)) {
+      event.preventDefault()
+      event.stopPropagation()
+      if (!pressed && !event.repeat) {
+        setPressed(true)
+      }
+    }
+  }
+
+  const onKeyUp = (event: React.KeyboardEvent<HTMLElement>) => {
+    if (isValidKeyboardEvent(event.nativeEvent) && !event.repeat) {
+      setPressed(false)
+    }
+  }
+
+  return {
+    pressed,
+    pressProps: { onKeyDown, onKeyUp, onClick, onMouseDown, onMouseUp, onMouseLeave },
+  }
+}
+
+function isValidKeyboardEvent(event: KeyboardEvent): boolean {
+  const { key, target } = event
+  const element = target as HTMLElement
+  const { tagName } = element
+
+  const role = element.getAttribute('role')
+  const isHTMLAnchorLink = element.tagName === 'A' && element.hasAttribute('href')
+
+  // Обрабатываем только случаи нажатия `Space` и `Enter` (Spacebar используется для ie11).
+  return (
+    (key === 'Enter' || key === ' ' || key === 'Spacebar') &&
+    tagName !== 'INPUT' &&
+    tagName !== 'TEXTAREA' &&
+    // Элемент-ссылка должен обрабатываться нативно, если только это не `role=button`.
+    (!isHTMLAnchorLink || (role === 'button' && key !== 'Enter')) &&
+    // Элемент с `role=link` должен обрабатываться только с клавишей `Enter`.
+    !(role === 'link' && key !== 'Enter')
+  )
+}
