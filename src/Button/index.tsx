421f423ff78952400619956228eeb4c4fbfb5dd1
yarastqt <yarastqt@gmail.com>
2020-12-13 22:10:29 +0300

    chore: corret typo Prosp -> Props

--- src/Button/index.tsx
+++ src/Button/index.tsx
@@ -2,13 +2,13 @@ import React, { FC, useRef } from 'react'
 
 import { useButton } from '../useButton'
 
-export type ButtonProsp = {
+export type ButtonProps = {
   disabled?: boolean
 }
 
-export const Button: FC<ButtonProsp> = ({ children, disabled }) => {
+export const Button: FC<ButtonProps> = ({ children, disabled, ...props }) => {
   const ref = useRef<HTMLButtonElement>(null)
-  const { isPressed, buttonProps } = useButton({ isDisabled: disabled })
+  const { isPressed, buttonProps } = useButton({ isDisabled: disabled, ...props })
 
   return (
     <button {...buttonProps} ref={ref} data-pressed={isPressed}>
