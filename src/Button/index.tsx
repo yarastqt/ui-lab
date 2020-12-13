1a319b2c680a8fe1435c72f5c0ae83fd284d16dd
yarastqt <yarastqt@gmail.com>
2020-12-13 22:16:39 +0300

    feat(Button): impl new layout with feature

--- src/Button/index.tsx
+++ src/Button/index.tsx
@@ -1,5 +1,6 @@
 import React, { FC, useRef } from 'react'
 
+import { BUTTON_NEW_LAYOUT } from '../features'
 import { useButton } from '../useButton'
 
 export type ButtonProps = {
@@ -9,6 +10,16 @@ export type ButtonProps = {
 export const Button: FC<ButtonProps> = ({ children, disabled, ...props }) => {
   const ref = useRef<HTMLButtonElement>(null)
   const { isPressed, buttonProps } = useButton({ isDisabled: disabled, ...props })
+
+  if (process.env[BUTTON_NEW_LAYOUT.value]) {
+    return (
+      <button {...buttonProps} ref={ref} data-pressed={isPressed}>
+        <span>addonBefore</span>
+        <span>{children}</span>
+        <span>addonAfter</span>
+      </button>
+    )
+  }
 
   return (
     <button {...buttonProps} ref={ref} data-pressed={isPressed}>
---
+++ src/features.ts
@@ -0,0 +1,4 @@
+export const BUTTON_NEW_LAYOUT = {
+  value: 'BUTTON_NEW_LAYOUT',
+  description: 'Новая верстка кнопки.',
+};
